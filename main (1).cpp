80892af159b56c68cd81d22807f07c7a6949bb74
helio000 <160642659+helio000@users.noreply.github.com>
2024-03-12 14:52:27 -0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, z;
+    printf("digite tres numeros inteiros\n");
+    scanf("%d" , &x);
+   
+   
+    printf("digite o segundo numero y:");
+    scanf("%d" , &y);
+    z = (x + y);
+    
+    printf("a soma de x + y e igual a ");
+    printf("%d" ,z);
+    
+    
+}
